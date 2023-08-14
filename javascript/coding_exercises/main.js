7169f189c838b3c9c8491c722d4653012b7684c9
RngValue <104089072+RngValue@users.noreply.github.com>
2023-08-15 00:01:54 +0200

    Update main.js

--- javascript/coding_exercises/main.js
+++ javascript/coding_exercises/main.js
@@ -1,20 +1,23 @@
-function less_than_hundret(a, b) {
+//"Less Than 100?" https://edabit.com/challenge/9MjEpkL7yAjAqiH58
+function less_than_hundred(a, b) {
     return a + b < 100;
 }
 
+//"Seven Boom!" https://edabit.com/challenge/6R6gReGTGwzpwuffD
 function seven_boom(a) {
     if (a.includes(7)){ return "boom!"; } else return "there is no 7 in the array.";
 }
 
+//"Number of Boomerangs" https://edabit.com/challenge/b7iHQDw72zzkmgCun
 var numberOfBoomerangs = 0;
 function boomerangs(a) {
     for (var i = 0; i < a.length; i++) {
-        if (a[i+2] === a[i]) numberOfBoomerangs++;
-        if (i+2 === a.length) break;
+        if (a[i+2] == a[i] && a[i+1] != a[i]) numberOfBoomerangs++;
+        if (i+2 == a.length) break;
     }
     return numberOfBoomerangs;
 }
 
-console.log(String(less_than_hundret(10,15)));
+console.log(String(less_than_hundred(10,15)));
 console.log(seven_boom([1,2,3,4,5,6]));
-console.log(String(boomerangs([2,1,2,1,2,1,2,1])));
+console.log(String(boomerangs([2,1,2,1,2,1,1,1])));
