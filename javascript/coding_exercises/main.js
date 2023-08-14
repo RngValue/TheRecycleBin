ad48c97d14fd9f0c9b5eeb7ec5b498346441d211
RngValue <104089072+RngValue@users.noreply.github.com>
2023-08-14 22:32:05 +0200

    a coding exercises

--- javascript/coding_exercises/main.js
+++ javascript/coding_exercises/main.js
@@ -6,5 +6,15 @@ function seven_boom(a) {
     if (a.includes(7)){ return "boom!"; } else return "there is no 7 in the array.";
 }
 
-//console.log(String(less_than_hundret(numberOne,numberTwo)));
+var numberOfBoomerangs = 0;
+function boomerangs(a) {
+    for (var i = 0; i < a.length; i++) {
+        if (a[i+2] === a[i]) numberOfBoomerangs++;
+        if (i+2 === a.length) break;
+    }
+    return numberOfBoomerangs;
+}
+
+console.log(String(less_than_hundret(numberOne,numberTwo)));
 console.log(seven_boom([1,2,3,4,5,6]));
+console.log(String(boomerangs([2,1,2,1,2,1,2,1])));
