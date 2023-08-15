71dddb52bec60b0ec0c8d1f58c8b2771aa5ed10a
RngValue <104089072+RngValue@users.noreply.github.com>
2023-08-15 19:04:49 +0200

    Update main.lua

--- lua/experimentations/main.lua
+++ lua/experimentations/main.lua
@@ -2,7 +2,10 @@ b = [[
 I have granted kids to hell.
 Oh no, not again!]]
 
-g = io.read(1)
-print(g)
+io.write("Type in whatever character: ")
+g = io.read("*l")
+io.write("Press [ENTER]")
+io.read()
+print(string.sub(g, 1, 1))
 print(b)
 io.write("va", type(a), "la\n") -- Lua is kinda based
