00dbc902a49826d6a22283174bb6270a96cad6bd
RngValue <104089072+RngValue@users.noreply.github.com>
2023-08-17 19:35:34 +0200

    rust

--- rust/sun_or_one_trillion_blank/src/main.rs
+++ rust/sun_or_one_trillion_blank/src/main.rs
@@ -1,4 +1,16 @@
+use std::io;
+
 fn main() {
-    let mut userInput = read_one();
-    println!("{userInput}");
+    let power_of_the_sun = 400;
+    let mut final_power = 0;
+
+    let mut user_input = String::new();
+
+    let _ = io::stdin().read_line(&mut user_input);
+    for character in user_input.chars() {
+        final_power += character as u32;
+    }
+    println!("The power of 1 {user_input} is equel to {final_power}.");
+    println!("The power of 1 trillion {user_input}s is equel to {final_power} trillion.");
+    println!("")
 }
