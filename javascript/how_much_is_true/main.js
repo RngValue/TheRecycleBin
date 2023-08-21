bd83769ffe2375a1ad24657c321794b544c88d2e
RngValue <104089072+RngValue@users.noreply.github.com>
2023-08-21 17:28:02 +0200

    Update main.js

--- javascript/how_much_is_true/main.js
+++ javascript/how_much_is_true/main.js
@@ -1,7 +1,14 @@
 //How Much is True? (https://edabit.com/challenge/GLbuMfTtDWwDv2F73)
 
-function cout_true() {
-    
+function count_true(booleans) {
+    let numberOfTrues = 0;
+    if (booleans == null) booleans = false
+    for (var i = 0; i < booleans.length; i++) {
+        if (booleans[i] == true) numberOfTrues++;
+    }
+    return numberOfTrues;
 }
 
+console.log(count_true([true, false, false, true, false]));
+console.log(count_true([false, false, false, false]));
 console.log(count_true());
