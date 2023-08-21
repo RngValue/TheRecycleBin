aa0e4033046220441fe53ac06c6416f672ac498c
RngValue <104089072+RngValue@users.noreply.github.com>
2023-08-21 16:52:59 +0200

    Greater Than the Sum?

--- cpp/greater_than_the_sum/main.cpp
+++ cpp/greater_than_the_sum/main.cpp
@@ -1,6 +1,24 @@
+//Greater Than the Sum? (https://edabit.com/challenge/uCxezzuDTdN7KaGCi)
 #include <iostream>
 
-int main(void) {
+using std::cout;
 
+bool greater_than_sum(int *numbers, int size) {
+    int sum = 0;
+    for (int i = 1; i <= size; i++) {
+        sum += numbers[i-1];
+        if (numbers[i] < sum) {
+            return false;
+            break;
+        }
+    }
+    return true;
+}
+
+int main(void) {
+    int a[] = {2, 3, 7, 13, 28};
+    cout << greater_than_sum(a, 5) << "\n";
+    int b[] = {1, 2, 4, 6, 13};
+    cout << greater_than_sum(b, 5) << "\n";
     return 0;
 }
