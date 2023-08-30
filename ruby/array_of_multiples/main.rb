d188fa59fbdb2d6b46cb88cccd18e167bad105a3
RngValue <104089072+RngValue@users.noreply.github.com>
2023-08-30 19:04:22 +0200

    Update main.rb

--- ruby/array_of_multiples/main.rb
+++ ruby/array_of_multiples/main.rb
@@ -1,2 +1,19 @@
 # Array of Multiples (https://edabit.com/challenge/X4nWvwWCqWYyGz6Ci)
 
+def array_of_multiples (num, length)
+	number = num
+    arrayOfNumbers = []
+    for i in 1..length do
+        number = i * num
+        arrayOfNumbers << number
+        i+=1
+    end
+    return arrayOfNumbers
+end
+
+print array_of_multiples(7, 5)
+puts " "
+print array_of_multiples(12, 10)
+puts " "
+print array_of_multiples(17, 6)
+puts " "
