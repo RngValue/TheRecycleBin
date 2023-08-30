c55d7a26e3cac0c48160439023d7b14e6eba7893
RngValue <104089072+RngValue@users.noreply.github.com>
2023-08-30 18:46:02 +0200

    Update main.rb

--- ruby/age_to_days/main.rb
+++ ruby/age_to_days/main.rb
@@ -1,3 +1,10 @@
 # Convert Age to Days (https://edabit.com/challenge/sDTLJtdpSvkZY6kjy)
 # Ignores the second note for the sake of my sanity...
 
+def calc_age(age)
+	return age * 365
+end
+
+puts calc_age(65)
+puts calc_age(0)
+puts calc_age(20)
