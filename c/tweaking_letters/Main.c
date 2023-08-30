c02ed1b0259d550721c0100250f3248f2882c7c2
RngValue <104089072+RngValue@users.noreply.github.com>
2023-08-30 19:17:00 +0200

    Delete Main.c

--- c/tweaking_letters/Main.c
+++
@@ -1,8 +0,0 @@
-// Tweaking Letters (https://edabit.com/challenge/868Q9JmTSAEhZBBrg)
-
-char *characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-char *e;
-int index;
-
-e = strchr(string, 'e');
-index = (int)(e - string);
