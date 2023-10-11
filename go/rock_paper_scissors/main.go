189b9d690a51107e9da6953644fff86f2f6798f0
RngValue <104089072+RngValue@users.noreply.github.com>
2023-10-11 13:18:02 +0200

    go

---
+++ go/rock_paper_scissors/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func rps(playerOne string, playerTwo string) string {
+  var a string = ""
+  if playerOne == playerTwo {
+    a = "TIE"
+  } else if playerOne == "rock" && playerTwo == "paper" {
+    a = "player 2 wins"
+  } else if playerOne == "paper" && playerTwo == "rock" {
+    a = "player 1 wins"
+  } else if playerOne == "scissors" && playerTwo == "rock" {
+    a = "player 2 wins"
+  } else if playerOne == "rock" && playerTwo == "scissors" {
+    a = "player 1 wins"
+  } else if playerOne == "paper" && playerTwo == "scissors" {
+    a = "player 2 wins"
+  } else if playerOne == "scissors" && playerTwo == "paper" {
+    a = "player 1 wins"
+  }
+  return a
+}
+
+func main() {
+  fmt.Println(rps("rock", "paper"))
+  fmt.Println(rps("paper", "rock"))
+  fmt.Println(rps("paper", "scissors"))
+  fmt.Println(rps("scissors", "scissors"))
+  fmt.Println(rps("scissors", "paper"))
+}
