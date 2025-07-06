9f8f604fe3f83e190f2ef31319ee2fa5bd9a6c42
CursedCodMan <104089072+RngValue@users.noreply.github.com>
2025-07-06 15:21:38 +0200

    Update main.js

--- javascript/christmas_tree/main.js
+++ javascript/christmas_tree/main.js
@@ -2,7 +2,7 @@
 
 function tree(h) {
 	let letree = []
-	for (let i = 1; i <= h; i++) letree.push("#".repeat(i))
+	for (let i = 1; i <= h; i++) letree.push("#".repeat(i) + "#".repeat(Math.abs(i-1)))
 	return letree
 }
 
